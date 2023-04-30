43740cd752a1680fae0bdc4fa14673a96b37b4db
utran <utran.host@qq.com>
2023-04-30 20:09:59 +0800

    增加subscribe 和unsubscribe 方法,并实现在断线后执行重新订阅的逻辑

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,6 @@
-import { UtType, UtRequest, Futrue, FullRequestFuture, UtResponse, LoaclWaitTimoutError, ClientState, FullRequstFutureCache, UtState, FullRequest } from '../object'
+import { UtType, UtRequest, Futrue, FullRequestFuture, UtResponse, LoaclWaitTimoutError, ClientState, FullRequstFutureCache, UtState, FullRequest, TopicHandlerCollection, SubscribeResult, RequestParamError } from '../object'
 import { basicAuth, isErrorInstanceOf } from '../utils'
 import { UtSocket } from './socket'
-
 
 export class BaseClient {
   private socket: UtSocket
@@ -11,10 +10,12 @@ export class BaseClient {
   private readonly fullRequstFutureCache: FullRequstFutureCache = new FullRequstFutureCache()
   private readonly maxReconnectNum: number
   private readonly state: ClientState = new ClientState(UtState.UN_START)
-  constructor (url: string, usename: string = 'utranhost', password: string = 'utranhost', maxReconnectNum: number = 3) {
+  private readonly topicHandlers: TopicHandlerCollection = new TopicHandlerCollection()
+
+  constructor (url: string, usename: string = 'utranhost', password: string = 'utranhost', maxReconnectNum: number = 32) {
     const token = basicAuth(usename, password)
     this.url = `${url}?Authorization=${token}`
-    this.socket = new UtSocket(this.url, this.onDisconnect.bind(this))
+    this.socket = new UtSocket(this.url, this.onDisconnect.bind(this), this.onPublish.bind(this))
     this.maxReconnectNum = maxReconnectNum
   }
 
@@ -29,31 +30,33 @@ export class BaseClient {
     }
   }
 
-  private genRequestId (): number {
+  public genRequestId (): number {
     this.requestId++
     return this.requestId
   }
 
-  private onDisconnect (isSafeExit:boolean, reconnectNum: number = 0): void {    
+  private onDisconnect (isSafeExit: boolean, reconnectNum: number = 0): void {
     const self = this
     const faildRequest = self.fullRequstFutureCache.getAllUtRequest()
     self.reconnectFuture = new Futrue<boolean, never, never>()
-    if(isSafeExit){
+    if (isSafeExit) {
       // 安全退出
       self.state.changeState(UtState.RECONNECT_FAILDE)
-      self.fullRequstFutureCache.setAllRequest2Faild(`请求发送失败，原因:执行安全退出`)
-      self.reconnectFuture.setResult(false)      
+      self.fullRequstFutureCache.setAllRequest2Faild('请求发送失败，原因:执行安全退出')
+      self.reconnectFuture.setResult(false)
       return
     }
-    
+
     console.warn(`尝试重连:${reconnectNum}/${self.maxReconnectNum}`)
     self.state.changeState(UtState.DISCONECTION)
-    new UtSocket(this.url, this.onDisconnect.bind(this)).start().then(socket => {
+    new UtSocket(this.url, this.onDisconnect.bind(this), this.onPublish.bind(this)).start().then(socket => {
       // 重连成功
       self.socket = socket
       self.state.changeState(UtState.RUNING)
+      // 重新订阅
+      self.reSubscribe().finally(() => {})
+      // 重新发起请求
       faildRequest.forEach(request => {
-        // 重新发起请求
         socket.send(request)
           .then(response => {
             // 请求发送成功
@@ -87,9 +90,41 @@ export class BaseClient {
     })
   }
 
-  
-  public async sendRequest (fullRequest:FullRequest): Promise<UtResponse> {
+  private onPublish (topic: string, msg: any): void {
+    this.topicHandlers.getCallback(topic).forEach((cb) => {
+      cb(topic, msg)
+    })
+  }
+
+  private async reSubscribe (): Promise<boolean> {
+    // 重新订阅
     const id = this.genRequestId()
+    const topics = this.topicHandlers.getAllTopics()
+    if (topics.length === 0) return true
+    console.log('重新订阅:', topics)
+    try {
+      const request: UtRequest = { id, requestType: UtType.SUBSCRIBE, topics }
+      const response = await this.socket.send(request)
+      if (response.state === 1) {
+        const { allTopics, subTopics } = response.result as SubscribeResult
+        const diff = topics.filter(item => {
+          return !allTopics.includes(item)
+        })
+        console.log('已重新订阅:', subTopics)
+        if (diff.length !== 0) {
+          console.error('有未成功的订阅:', diff)
+          return false
+        }
+        return true
+      }
+      return false
+    } catch (error) {
+      return false
+    }
+  }
+
+  public async sendRequest (fullRequest: FullRequest): Promise<UtResponse> {
+    const id = fullRequest.id
     const fullreqFutrue = new FullRequestFuture(fullRequest)
     if (this.state.getState() !== UtState.RUNING) {
       return fullreqFutrue.setRequest2Faild(`请求发送失败，原因:${this.state.getMsg()}`)
@@ -105,81 +140,64 @@ export class BaseClient {
         this.fullRequstFutureCache.pop(id)
         return fullreqFutrue.setRequest2Faild(`${this.state.getMsg()}`)
       }
+      if (isErrorInstanceOf(error, RequestParamError)) {
+        throw error
+      }
       return await fullreqFutrue.getResult()
     }
   }
 
-  public exit(){
+  public async subscribe (topic: string, callback: (topic: string, msg: any) => void, timeout?: number, isReSub: boolean = false): Promise<UtResponse> {
+    const id = this.genRequestId()
+    const fullRequest: FullRequest = { id, requestType: UtType.SUBSCRIBE, topics: [topic], timeout }
+    const response = await this.sendRequest(fullRequest)
+    if (response.state === 1 && !isReSub) {
+      // 添加订阅
+      this.topicHandlers.add([topic, callback])
+    }
+    return response
+  }
+
+  public async unsubscribe (topics: string[], timeout?: number): Promise<UtResponse> {
+    const id = this.genRequestId()
+    const fullRequest: FullRequest = { id, requestType: UtType.UNSUBSCRIBE, topics, timeout }
+    const response = await this.sendRequest(fullRequest)
+    if (response.state === 1) {
+      // 取消订阅
+      const self = this
+      topics.forEach((topic) => {
+        self.topicHandlers.remove(topic)
+      })
+    }
+    return response
+  }
+
+  public exit (): void {
     try {
       this.socket.exit()
     } catch (error) {
       this.onDisconnect(true)
     }
-    console.log(`安全退出.`)
+    console.log('安全退出.')
   }
 }
 
 
+// class ClienProxy {
+//   start:(...args)=> Promise<boolean>
+//   call:(...args)=> Promise<UtResponse>
+//   multicall:(...args)=> Promise<UtResponse[]>
+//   subscribe:(...args)=> Promise<UtResponse>
+//   unsubscribe:(...args)=>Promise<UtResponse>
+//   exit:()=>void
+// }
 
-class Clien{
-    // public async multicall (timeout?:number,...requests: UtRequest[]): Promise<UtResponse[]> {
-  //   const self = this
-  //   const id = this.genRequestId()
-  //   const fullreqFutrue = new FullRequestFuture({ id, requestType, methodName, args, dicts, timeout })
-  //   const calls = requests.map(request=>{
-  //     return self.socket.send(request)
-  //   })
-  //   try {
-  //     const responses = await Promise.all(calls)
+// function Client(){
+//   new BaseClient()
+//   // new Proxy(BaseClient,{
+//   //   get(target, p, receiver) {
 
-  //     return responses
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-    
+//   //   },
+//   // })
 
-  // }
-
-  // public async subscribe (topic: string, callback: (topic: string, msg: any) => void): Promise<any> {
-  //   if (!this.isruning) await this.holdingReconnect
-  //   return await this.bsclient.subscribe(topic, callback)
-  // }
-
-  // public async unsubscribe (topic: string[]): Promise<any> {
-  //   if (!this.isruning) await this.holdingReconnect
-  //   return await this.bsclient.unsubscribe(topic)
-  // }
-
-  // private reconnection (code: number): void {
-  //   if (code === SAFE_EIXTCODE) return
-  //   const self = this
-  //   const oldClient = this.bsclient
-  //   this.holdingReconnect = new Promise((resolve, reject) => {
-  //     console.warn('断线重连中..')
-  //     this.isruning = false
-  //     self.start()
-  //       .then(res => {
-  //         console.log('重连成功')
-  //         const topicHandlers = oldClient.getTopicHandlers()
-  //         if (topicHandlers.length > 0) {
-  //           // 重新订阅topic
-  //           const p = topicHandlers.map(async ([topic, handler]) => {
-  //             return await self.bsclient.subscribe(topic, handler)
-  //           })
-  //           Promise.all(p)
-  //             .then((res) => {
-  //               console.log(`已重新订阅：${String(topicHandlers.map(([topic, _]) => { return topic }))}`)
-  //               resolve(res)
-  //             })
-  //             .catch((err) => {
-  //               console.warn(`重新订阅时失败，${String(err)}`)
-  //               reject(err)
-  //             })
-  //         } else {
-  //           // 没有重订的topic
-  //           resolve(res)
-  //         }
-  //       }).catch(err => { reject(err) })
-  //   })
-  // }
-}
+// }
