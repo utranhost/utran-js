419c7677cae11db8329df71cf3020ab580c86507
utran <utran.host@qq.com>
2023-05-02 00:18:19 +0800

    1. 新增multicall方法; 2.增加可选项:   timeout?: number   outputResult?: boolean   defaultResult?: any   throwTimeoutError?: boolean 3. 增加判断响应是否为断线响应 BreakErrorMsg的逻辑

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,19 +1,19 @@
-import { UtType, UtRequest, Futrue, FullRequestFuture, UtResponse, LoaclWaitTimoutError, ClientState, FullRequstFutureCache, UtState, FullRequest, TopicHandlerCollection, SubscribeResult, RequestParamError } from '../object'
-import { basicAuth, isErrorInstanceOf } from '../utils'
-import { UtSocket } from './socket'
+import { UtType, UtRequest, Futrue, FullRequestFuture, UtResponse, ClientState, FullRequstFutureCache, UtState, FullRequest, TopicHandlerCollection, SubscribeResult, RequestParamError, LoaclWaitTimoutError } from '../object'
+import { basicAuth, isErrorInstanceOf, parameterDeconstruction } from '../utils'
+import { BreakErrorMsg, UtSocket } from './socket'
 
 export class BaseClient {
   private socket: UtSocket
   private readonly url: string
   private requestId: number = 0
   private reconnectFuture: Futrue<boolean, never, never>
-  private readonly fullRequstFutureCache: FullRequstFutureCache = new FullRequstFutureCache()
+  protected readonly fullRequstFutureCache: FullRequstFutureCache = new FullRequstFutureCache()
   private readonly maxReconnectNum: number
   private readonly state: ClientState = new ClientState(UtState.UN_START)
   private readonly topicHandlers: TopicHandlerCollection = new TopicHandlerCollection()
 
-  constructor (url: string, usename: string = 'utranhost', password: string = 'utranhost', maxReconnectNum: number = 32) {
-    const token = basicAuth(usename, password)
+  constructor (url: string, username: string = 'utranhost', password: string = 'utranhost', maxReconnectNum: number = 32) {
+    const token = basicAuth(username, password)
     this.url = `${url}?Authorization=${token}`
     this.socket = new UtSocket(this.url, this.onDisconnect.bind(this), this.onPublish.bind(this))
     this.maxReconnectNum = maxReconnectNum
@@ -49,45 +49,46 @@ export class BaseClient {
 
     console.warn(`尝试重连:${reconnectNum}/${self.maxReconnectNum}`)
     self.state.changeState(UtState.DISCONECTION)
-    new UtSocket(this.url, this.onDisconnect.bind(this), this.onPublish.bind(this)).start().then(socket => {
+    new UtSocket(this.url, this.onDisconnect.bind(this), this.onPublish.bind(this)).start()
+      .then(socket => {
       // 重连成功
-      self.socket = socket
-      self.state.changeState(UtState.RUNING)
-      // 重新订阅
-      self.reSubscribe().finally(() => {})
-      // 重新发起请求
-      faildRequest.forEach(request => {
-        socket.send(request)
-          .then(response => {
+        self.socket = socket
+        self.state.changeState(UtState.RUNING)
+        // 重新订阅
+        self.reSubscribe().finally(() => {})
+        // 重新发起请求
+        faildRequest.forEach(request => {
+          socket.send(request)
+            .then(response => {
             // 请求发送成功
-            self.state.changeState(UtState.RUNING)
-            const fullRequestFuture = self.fullRequstFutureCache.pop(response.id)
-            if (fullRequestFuture !== null) {
-              fullRequestFuture.setResult(response)
-            }
-          })
-          .catch(err => {
+              self.state.changeState(UtState.RUNING)
+              const fullRequestFuture = self.fullRequstFutureCache.pop(response.id)
+              if (fullRequestFuture !== null) {
+                if (fullRequestFuture.isPending) fullRequestFuture.setResult(response)
+              }
+            })
+            .catch(err => {
             // 请求发送失败
-            self.state.changeState(UtState.DISCONECTION)
-            console.warn(`重发[${request.requestType}]${request.id}请求失败:${String(err)}`)
-          })
+              self.state.changeState(UtState.DISCONECTION)
+              console.warn(`重发[${request.requestType}]${request.id}请求失败:${String(err)}`)
+            })
+        })
+        self.reconnectFuture.setResult(true)
+      }).catch(err => {
+        console.warn(`连接失败，剩余重连：${reconnectNum}/${self.maxReconnectNum}`)
+        if (reconnectNum < self.maxReconnectNum) {
+          self.state.changeState(UtState.RECONNECTING)
+          reconnectNum++
+          setTimeout(() => {
+            self.onDisconnect(false, reconnectNum)
+          }, 1000 * Math.log(reconnectNum) * 2)
+        } else {
+          self.state.changeState(UtState.RECONNECT_FAILDE)
+          self.fullRequstFutureCache.setAllRequest2Faild(`请求发送失败，原因:${self.state.getMsg()}`)
+          self.reconnectFuture.setResult(false)
+          console.error(`连接失败，结束重连.\n${String(err)}`)
+        }
       })
-      self.reconnectFuture.setResult(true)
-    }).catch(err => {
-      console.warn(`连接失败，剩余重连：${reconnectNum}/${self.maxReconnectNum}`)
-      if (reconnectNum < self.maxReconnectNum) {
-        self.state.changeState(UtState.RECONNECTING)
-        reconnectNum++
-        setTimeout(() => {
-          self.onDisconnect(false, reconnectNum)
-        }, 1000 * Math.log(reconnectNum) * 2)
-      } else {
-        self.state.changeState(UtState.RECONNECT_FAILDE)
-        self.fullRequstFutureCache.setAllRequest2Faild(`请求发送失败，原因:${self.state.getMsg()}`)
-        self.reconnectFuture.setResult(false)
-        console.error(`连接失败，结束重连.\n${String(err)}`)
-      }
-    })
   }
 
   private onPublish (topic: string, msg: any): void {
@@ -123,6 +124,29 @@ export class BaseClient {
     }
   }
 
+  public sendRequest2 (fullRequest: FullRequest): FullRequestFuture {
+    const id = fullRequest.id
+    const self = this
+    const fullreqFutrue = new FullRequestFuture(fullRequest)
+    if (self.state.getState() !== UtState.RUNING) {
+      fullreqFutrue.setRequest2Faild(`请求发送失败，原因:${self.state.getMsg()}`)
+      return fullreqFutrue
+    }
+    self.fullRequstFutureCache.push(id, fullreqFutrue)
+    self.socket.send(fullreqFutrue.getUtRequest(), fullRequest.timeout)
+      .then(response => {
+        if (response.error === BreakErrorMsg) return
+        if (fullreqFutrue.isPending) fullreqFutrue.setResult(response)
+        self.fullRequstFutureCache.pop(id)
+      })
+      .catch(error => {
+        if (isErrorInstanceOf(error, RequestParamError)) {
+          if (fullreqFutrue.isPending) fullreqFutrue.setResult(error)
+        }
+      })
+    return fullreqFutrue
+  }
+
   public async sendRequest (fullRequest: FullRequest): Promise<UtResponse> {
     const id = fullRequest.id
     const fullreqFutrue = new FullRequestFuture(fullRequest)
@@ -132,14 +156,11 @@ export class BaseClient {
     this.fullRequstFutureCache.push(id, fullreqFutrue)
     try {
       const response = await this.socket.send(fullreqFutrue.getUtRequest(), fullRequest.timeout)
-      fullreqFutrue.setResult(response)
+      if (response.error === BreakErrorMsg) return await fullreqFutrue.getResult()
+      if (fullreqFutrue.isPending) fullreqFutrue.setResult(response)
       this.fullRequstFutureCache.pop(id)
       return response
     } catch (error) {
-      if (isErrorInstanceOf(error, LoaclWaitTimoutError)) {
-        this.fullRequstFutureCache.pop(id)
-        return fullreqFutrue.setRequest2Faild(`${this.state.getMsg()}`)
-      }
       if (isErrorInstanceOf(error, RequestParamError)) {
         throw error
       }
@@ -182,22 +203,117 @@ export class BaseClient {
   }
 }
 
+export interface UtOptions{
+  timeout?: number
+  outputResult?: boolean
+  defaultResult?: any
+  throwTimeoutError?: boolean
+}
 
-// class ClienProxy {
-//   start:(...args)=> Promise<boolean>
-//   call:(...args)=> Promise<UtResponse>
-//   multicall:(...args)=> Promise<UtResponse[]>
-//   subscribe:(...args)=> Promise<UtResponse>
-//   unsubscribe:(...args)=>Promise<UtResponse>
-//   exit:()=>void
-// }
+export interface InitOptions{
+  username?: string
+  password?: string
+  maxReconnectNum?: number
+}
 
-// function Client(){
-//   new BaseClient()
-//   // new Proxy(BaseClient,{
-//   //   get(target, p, receiver) {
+export type UtClientInitOptions = UtOptions & InitOptions
 
-//   //   },
-//   // })
+export interface MiniRequest {
+  methodName: string
+  params: any[]
+}
 
-// }
+export class UtClient<R=UtResponse> extends BaseClient {
+  protected tempOpts: UtOptions
+  private readonly timeout?: number
+  private readonly outputResult: boolean
+  private readonly defaultResult: any
+  private readonly throwTimeoutError: boolean
+  constructor (
+    url: string,
+    utclientInitOptions: UtClientInitOptions = {}) {
+    const { username = 'utranhost', password = 'utranhost', maxReconnectNum = 32, timeout, outputResult = false, defaultResult, throwTimeoutError = false } = utclientInitOptions
+    super(url, username, password, maxReconnectNum)
+    this.timeout = timeout
+    this.outputResult = outputResult
+    this.defaultResult = defaultResult
+    this.throwTimeoutError = throwTimeoutError
+  }
+
+  public setOptions (opts: UtOptions): UtClient {
+    this.tempOpts = opts
+    return this as UtClient
+  }
+
+  public getOptions (isclear: boolean = true): UtOptions {
+    let { timeout, outputResult, defaultResult, throwTimeoutError } = this.tempOpts
+    timeout = timeout === undefined ? this.timeout : timeout
+    outputResult = outputResult === undefined ? this.outputResult : outputResult
+    defaultResult = defaultResult === undefined ? this.defaultResult : defaultResult
+    throwTimeoutError = throwTimeoutError === undefined ? this.throwTimeoutError : throwTimeoutError
+    if (isclear) this.tempOpts = {}
+    return { timeout, outputResult, defaultResult }
+  }
+
+  public async callByName (methodName: string, ...params): Promise<R | FullRequest> {
+    const { timeout, outputResult, defaultResult } = this.getOptions()
+    const { args, dicts } = parameterDeconstruction(...params)
+    const id = this.genRequestId()
+    const fullrequest = { id, requestType: UtType.RPC, methodName, args, dicts, timeout }
+    const response = await this.sendRequest(fullrequest)
+
+    if (outputResult === true) {
+      return response.result === undefined ? defaultResult : response.result
+    } else {
+      return response as R
+    }
+  }
+
+  public async multicall (...miniRequests: MiniRequest[]): Promise<R[]> {
+    return await this._multicall(miniRequests)
+  }
+
+  private async _multicall (miniRequests: MiniRequest[]): Promise<R[]> {
+    const { timeout, outputResult, defaultResult, throwTimeoutError } = this.getOptions()
+    const self = this
+    const timeoutMsg = `multicall本地等待超时：${timeout as number}s`
+    const calls: Array<Promise<UtResponse>> = []
+    const callFutures: FullRequestFuture[] = []
+    let responses: any[] = []
+
+    let isTimeout: boolean = false
+    if (typeof timeout === 'number') setTimeout(() => { isTimeout = true }, timeout * 1000)
+
+    for (let index = 0; index < miniRequests.length; index++) {
+      if (isTimeout && throwTimeoutError === true) throw new LoaclWaitTimoutError(timeoutMsg)
+      const r = miniRequests[index]
+      const { args, dicts } = parameterDeconstruction(...r.params)
+      const methodName = r.methodName
+      const id = this.genRequestId()
+      const fullrequest = { id, requestType: UtType.RPC, methodName, args, dicts, timeout }
+      const futrue = self.sendRequest2(fullrequest)
+      callFutures.push(futrue)
+      calls.push(futrue.getResult())
+    }
+
+    for (let index = 0; index < calls.length; index++) {
+      if (isTimeout) {
+        if (throwTimeoutError === true) throw new LoaclWaitTimoutError(timeoutMsg)
+        responses = []
+        callFutures.forEach(fullRequestFuture => {
+          self.fullRequstFutureCache.pop(fullRequestFuture.getSource().id)
+          if (outputResult === true) {
+            responses.push(fullRequestFuture.isPending ? defaultResult : fullRequestFuture.result.result)
+          } else {
+            responses.push(fullRequestFuture.isPending ? fullRequestFuture.setRequest2Faild(timeoutMsg) : fullRequestFuture.result.result)
+          }
+        })
+        break
+      }
+      const response: UtResponse = await calls[index]
+      const result = outputResult !== true ? response : response.result !== undefined ? response.result : defaultResult
+      responses.push(result)
+    }
+    return responses
+  }
+}
